947dfed0600e5e48e62b335e9858281b5c3ea237
KMishik <k.mishik@gmail.com>
2018-04-13 15:29:11 +0500

    Release 2.0.2 Fixed NOO correction from 13.04.2018

--- src/Controller/UficContactsAjax.php
+++ src/Controller/UficContactsAjax.php
@@ -35,33 +35,33 @@ class UficContactsAjax extends ControllerBase
 
 	private function getTitle(&$title, $name) {
 		switch ($name) {
-			case 'ufic': $title = $this->t("ФГБНУ Уфимский федеральный исследовательский центр РАН");
+			case 'ufic': $title = $this->t("Уфимский федеральный исследовательский центр РАН");
 									break;
-			case 'imvc': $title = $this->t("ФГБНУ Институт математики с вычислительным центром УФИЦ РАН");
+			case 'imvc': $title = $this->t("Институт математики с вычислительным центром УФИЦ РАН");
 									break;
-			case 'imech': $title = $this->t("ФГБНУ Институт механики им. Р.Р.Мавлютова УФИЦ РАН");
+			case 'imech': $title = $this->t("Институт механики им. Р.Р.Мавлютова УФИЦ РАН");
 									break;
-			case 'ifmk': $title = $this->t("ФГБНУ Институт физики молекул и кристаллов УФИЦ РАН");
+			case 'ifmk': $title = $this->t("Институт физики молекул и кристаллов УФИЦ РАН");
 									break;
-			case 'ufih': $title = $this->t("ФГБНУ Уфимский Институт химии УФИЦ РАН");
+			case 'ufih': $title = $this->t("Уфимский Институт химии УФИЦ РАН");
 									break;
-			case 'ink': $title = $this->t("ФГБНУ Институт нефтехимии и катализа УФИЦ РАН");
+			case 'ink': $title = $this->t("Институт нефтехимии и катализа УФИЦ РАН");
 									break;
-			case 'ibio': $title = $this->t("ФГБНУ Уфимский Институт биологии УФИЦ РАН");
+			case 'ibio': $title = $this->t("Уфимский Институт биологии УФИЦ РАН");
 									break;
-			case 'ibg': $title = $this->t("ФГБНУ Институт биохимии и генетики УФИЦ РАН");
+			case 'ibg': $title = $this->t("Институт биохимии и генетики УФИЦ РАН");
 									break;
-			case 'botgarden': $title = $this->t("ФГБНУ Южно-Уральский ботанический сад-институт УФИЦ РАН");
+			case 'botgarden': $title = $this->t("Южно-Уральский ботанический сад-институт УФИЦ РАН");
 									break;
-			case 'bniish': $title = $this->t("ФГБНУ Башкирский НИИСХ УФИЦ РАН");
+			case 'bniish': $title = $this->t("Башкирский НИИСХ УФИЦ РАН");
 									break;
-			case 'igeo': $title = $this->t("ФГБНУ Институт геологии УФИЦ РАН");
+			case 'igeo': $title = $this->t("Институт геологии УФИЦ РАН");
 									break;
-			case 'isei': $title = $this->t("ФГБНУ Институт социально-экономических исследований УФИЦ РАН");
+			case 'isei': $title = $this->t("Институт социально-экономических исследований УФИЦ РАН");
 									break;
-			case 'ihll': $title = $this->t("ФГБНУ Ордена Знак Почета Институт истории, языка и литературы УФИЦ РАН");
+			case 'ihll': $title = $this->t("Ордена Знак Почета Институт истории, языка и литературы УФИЦ РАН");
 									break;
-			case 'iethno': $title = $this->t("ФГБНУ Институт этнологических исследований им. Р.Г.Кузеева УФИЦ РАН");
+			case 'iethno': $title = $this->t("Институт этнологических исследований им. Р.Г.Кузеева УФИЦ РАН");
 									break;
 			case 'polyclinic': $title = $this->t("ФГБУЗ Поликлиника УФИЦ РАН");
 									break;
